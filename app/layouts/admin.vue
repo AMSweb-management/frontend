ab09cbf06f361fe1d424b0b156a03a7ca125122e
Novalardi21 <novalsantria320@gmail.com>
2026-04-08 23:27:21 +0700

    fix: remove duplicate tailwind

---
+++ app/layouts/admin.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex min-h-screen bg-[#1E3A8A]">
+
+        <AdminSidebar class="w-64 shrink-0" />
+
+        <div class="flex-1 flex flex-col">
+
+            <AdminNavbar class="shrink-0" />
+
+            <main class="flex-1 overflow-y-auto p-8">
+                <slot />
+            </main>
+
+        </div>
+
+    </div>
+</template>
