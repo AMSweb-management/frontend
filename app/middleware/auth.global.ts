da0f33303f483dcb44f56f12076061494aa2074b
Novalardi21 <novalsantria320@gmail.com>
2026-04-20 23:19:30 +0700

    heheheh

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -1,18 +1,35 @@
 export default defineNuxtRouteMiddleware(async (to) => {
     const { user, token, fetchUser } = useAuth()
 
-    if (to.path === '/') return
+    // kalau halaman login
+    if (to.path === '/') {
+        // kalau sudah login → lempar ke dashboard
+        if (token.value) {
+            try {
+                if (!user.value) {
+                    await fetchUser()
+                }
+                return navigateTo('/dashboard')
+            } catch {
+                return
+            }
+        }
+        return
+    }
 
-    // kalau ada token tapi user belum ada → ambil user
-    if (!user.value && token.value) {
+    // halaman selain login (protected)
+
+    // kalau tidak ada token → balik login
+    if (!token.value) {
+        return navigateTo('/')
+    }
+
+    // kalau ada token tapi user kosong → ambil user
+    if (!user.value) {
         try {
             await fetchUser()
         } catch {
             return navigateTo('/')
         }
-    }
-
-    if (!token.value) {
-        return navigateTo('/')
     }
 })
